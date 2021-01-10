a77c83723f39df10ad8d9790f5f11ec44bc1c8b9
antonyoni <antony@reduc.tech>
2021-01-10 18:24:40 +0000

    Add spec for return_result Fix string concatentation issues for log. Add anchors to the expected regex.

--- spec/nuixconnectorscript_spec.rb
+++ spec/nuixconnectorscript_spec.rb
@@ -3,28 +3,32 @@ require 'nuixconnectorscript'
 include NuixConnectorScript
 
 describe 'log' do
+
   it 'logs a message with info severity by default' do
     expected = Regexp.escape('{"log":{"severity":"info","message":"message!","time":"')
-               + '.+'
-               + Regexp.escape('","stackTrace":""}}')
+                 .concat('.+')
+                 .concat(Regexp.escape('","stackTrace":""}}'))
     expect do
       log "message!"
-    end.to output(/#{expected}/).to_stdout
+    end.to output(/^#{expected}\r?\n$/).to_stdout
   end
+
   it 'logs a message with specified severity' do
     expected = Regexp.escape('{"log":{"severity":"error","message":"error!","time":"')
-               + '.+'
-               + Regexp.escape('","stackTrace":""}}')
+                 .concat('.+')
+                 .concat(Regexp.escape('","stackTrace":""}}'))
     expect do
       log("error!", severity: :error)
-    end.to output(/#{expected}/).to_stdout
+    end.to output(/^#{expected}\r?\n$/).to_stdout
   end
+
   it 'logs custom time and stacktrace if set' do
     expected = Regexp.escape('{"log":{"severity":"warn","message":"warning!","time":"time","stackTrace":"stack"}}')
     expect do
       log("warning!", severity: :warn, timestamp: 'time', stack: 'stack')
-    end.to output(/#{expected}\r?\n/).to_stdout
+    end.to output(/^#{expected}\r?\n$/).to_stdout
   end
+
   it 'does not log if severity is less than LOG_SEVERITY' do
     $VERBOSE = nil
     NuixConnectorScript::LOG_SEVERITY = :info
@@ -32,5 +36,15 @@ describe 'log' do
     expect do
       log("nothing please", severity: :debug)
     end.to_not output.to_stdout
+  end
+
+end
+
+describe 'return_result' do
+  it 'outputs result json to stdout' do
+    expected = Regexp.escape('{"result":{"data":"message!"}}')
+    expect do
+      return_result "message!"
+    end.to output(/^#{expected}\r?\n$/).to_stdout
   end
 end
