0bcc9b24d01556a16797b561871c6a1a4b26fce4
antonyoni <antony@reduc.tech>
2021-01-12 12:21:19 +0000

    Add unit tests for listen

--- spec/nuixconnectorscript_spec.rb
+++ spec/nuixconnectorscript_spec.rb
@@ -14,8 +14,8 @@ describe 'log' do
 
   it 'logs a message with info severity by default' do
     expected = Regexp.escape('{"log":{"severity":"info","message":"message!","time":"')
-                 .concat('.+')
-                 .concat(Regexp.escape('","stackTrace":""}}'))
+                     .concat('.+')
+                     .concat(Regexp.escape('","stackTrace":""}}'))
     expect do
       log "message!"
     end.to output(/^#{expected}\r?\n$/).to_stdout
@@ -23,8 +23,8 @@ describe 'log' do
 
   it 'logs a message with specified severity' do
     expected = Regexp.escape('{"log":{"severity":"error","message":"error!","time":"')
-                 .concat('.+')
-                 .concat(Regexp.escape('","stackTrace":""}}'))
+                     .concat('.+')
+                     .concat(Regexp.escape('","stackTrace":""}}'))
     expect do
       log("error!", severity: :error)
     end.to output(/^#{expected}\r?\n$/).to_stdout
@@ -120,7 +120,7 @@ describe 'write_error' do
     end
 
   end
-  
+
   context 'terminating' do
 
     before(:all) do
@@ -163,7 +163,7 @@ def run_listen(send = [])
     listen
   end
   sleep(0.1)
-  send.each { |msg| puts msg }
+  send.each { |msg| $stdout.puts msg }
   thread.join
 end
 
@@ -175,22 +175,116 @@ describe 'listen' do
   LOG_START = '\{"log":\{"severity":"info","message":"Starting","time":".+","stackTrace":""\}\}\r?\n'
   LOG_END   = '\{"log":\{"severity":"info","message":"Finished","time":".+","stackTrace":""\}\}\r?\n'
 
-  it 'returns when END_CMD is received' do
-    $VERBOSE = nil
-    NuixConnectorScript::END_CMD = 'end'
-    $VERBOSE = false
-    allow($stdin).to receive(:gets) { '{"cmd":"end"}' }
-    th = Thread.new {listen}
-    result = nil
-    expect($stdout).to receive(:puts).twice
-    expect{result = th.join(1)}.not_to raise_error
-    expect(result).to_not be_nil
-  end
+  #RSpec::Matchers.define_negated_matcher :not_output, :output
+
+  # it 'returns when END_CMD is received' do
+  #   $VERBOSE = nil
+  #   NuixConnectorScript::END_CMD = 'end'
+  #   $VERBOSE = false
+  #   allow($stdin).to receive(:gets) { '{"cmd":"end"}' }
+  #   th = Thread.new {listen}
+  #   result = nil
+  #   expect($stdout).to receive(:puts).twice
+  #   expect{result = th.join(1)}.not_to raise_error
+  #   expect(result).to_not be_nil
+  # end
 
   it 'logs start and end message' do
-    allow($stdin).to receive(:gets) { '{"cmd":"end"}' }
+    allow($stdin).to receive(:gets) { DONE_JSON }
     expected = LOG_START + LOG_END
     expect{run_listen}.to output(/^#{expected}$/).to_stdout
+  end
+
+  it 'runs function and returns a result message' do
+    func = "def get_result\\n  return 'hello'\\nend"
+    allow($stdin).to receive(:gets).twice.and_return(
+      "{\"cmd\":\"get_result\",\"def\":\"#{func}\"}",
+      DONE_JSON
+    )
+    expected = LOG_START + Regexp.escape('{"result":{"data":"hello"}}') + '\r?\n' + LOG_END
+    expect{run_listen}.to output(/^#{expected}$/).to_stdout
+  end
+
+  it 'uses stored def to run same function' do
+    func = "def get_result\\n  return 'hi'\\nend"
+    allow($stdin).to receive(:gets).exactly(3).and_return(
+      "{\"cmd\":\"get_result\",\"def\":\"#{func}\"}",
+      "{\"cmd\":\"get_result\"}",
+      DONE_JSON
+    )
+    expected = LOG_START +
+                 Regexp.escape('{"result":{"data":"hi"}}') +
+                 '\r?\n' +
+                 Regexp.escape('{"result":{"data":"hi"}}') +
+                 '\r?\n' +
+                 LOG_END
+    expect{run_listen}.to output(/^#{expected}$/).to_stdout
+  end
+
+  it 'replaces a function if a new def is provided' do
+    allow($stdin).to receive(:gets).exactly(3).and_return(
+      "{\"cmd\":\"get_result\",\"def\":\"def get_result\\n  return 'hi'\\nend\"}",
+      "{\"cmd\":\"get_result\",\"def\":\"def get_result\\n  return 'hello'\\nend\"}",
+      DONE_JSON
+    )
+    expected = LOG_START +
+                 Regexp.escape('{"result":{"data":"hi"}}') +
+                 '\r?\n' +
+                 Regexp.escape('{"result":{"data":"hello"}}') +
+                 '\r?\n' +
+                 LOG_END
+    expect{run_listen}.to output(/^#{expected}$/).to_stdout
+  end
+
+  it 'passes args to the function' do
+    func = "def write_out(args={})\\n  m = [args['1'], args['2']]\\n  return m.join(' ')\\nend"
+    allow($stdin).to receive(:gets).exactly(3).and_return(
+      "{\"cmd\":\"get_result\",\"def\":\"#{func}\",\"args\":{\"1\":\"hello\", \"2\":\"there!\"}}",
+      "{\"cmd\":\"get_result\",\"args\":{\"1\":\"bye\"}}",
+      DONE_JSON
+    )
+    expected = LOG_START +
+                 Regexp.escape('{"result":{"data":"hello there!"}}') +
+                 '\r?\n' +
+                 Regexp.escape('{"result":{"data":"bye "}}') +
+                 '\r?\n' +
+                 LOG_END
+    expect{run_listen}.to output(/^#{expected}$/).to_stdout
+  end
+
+  context 'errors' do
+
+    it "writes error when it can't parse json, and continues" do
+      allow($stdin).to receive(:gets).twice.and_return(
+        '{"cmd":"}',
+        DONE_JSON
+      )
+      expected_err = Regexp.escape('{"error":{"message":"Could not parse JSON: {\"cmd\":\"}"')
+      expected_log = Regexp.escape('{"log":{"severity":"error","message":"Could not parse JSON:')
+      expect{run_listen}.to output(/^#{expected_err}/).to_stderr.and \
+        output(/^#{expected_log}/).to_stdout
+    end
+
+    it "writes error when it can't find a function definition, and terminates" do
+      allow($stdin).to receive(:gets).once.and_return(
+        '{"cmd":"unknown"}'
+      )
+      expected_err = Regexp.escape('{"error":{"message":"Function definition for \'unknown\' not found"')
+      expected_log = Regexp.escape('{"log":{"severity":"error","message":"Function definition for \'unknown\' not found')
+      expect{run_listen}.to output(/^#{expected_err}/).to_stderr.and \
+        output(/^#{expected_log}/).to_stdout.and raise_error(SystemExit)
+    end
+
+    it "writes error when it can't execute a function, and terminates" do
+      allow($stdin).to receive(:gets).once.and_return(
+        "{\"cmd\":\"get_result\",\"def\":\"def get_result\\n  retrn 'hi'\\nend\"}"
+      )
+      expected_err = Regexp.escape('{"error":{"message":"Could not execute get_result:')
+      expected_log = Regexp.escape('{"log":{"severity":"error","message":"Could not execute get_result:')
+      expect{run_listen}.to output(/^#{expected_err}/).to_stderr.and \
+        output(/^#{expected_log}/).to_stdout.and raise_error(SystemExit)
+    end
+
   end
 
 end
